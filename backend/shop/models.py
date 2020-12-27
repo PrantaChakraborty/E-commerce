117d22448eb625f59ab184c2fe84372402814b76
PrantaChakraborty <cbpranta@gmail.com>
2020-12-27 15:49:28 +0600

    added Product and Category to admin

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -23,7 +23,8 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.BooleanField(default=True)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
